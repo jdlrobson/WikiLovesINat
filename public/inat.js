c72ef2e01cd83a58b76e5a3ead4568bc328db261
jdlrobson <jdlrobson@gmail.com>
2019-05-19 14:47:01 +0200

    Source more thumbnails for species from iNat
    Widen the net

--- public/inat.js
+++ public/inat.js
@@ -1,24 +1,40 @@
+function flatten(arr) {
+    return arr.reduce(function (flat, toFlatten) {
+      return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+    }, []);
+}
+
 const fetcher = {
-    fetchTaxa: function ( id ) {
-        return fetch( `https://api.inaturalist.org/v1/taxa/${id}` ).then((r) => {
+    fetchPhotos: ( id ) => {
+        return fetch( `https://api.inaturalist.org/v1/observations?verifiable=true&taxon_id=${id}&order_by=votes` ).then((r) => {
             return r.json();
         }).then((j) => {
             const results = j.results || [];
-            const result = results[0] || [];
-            const photos = ( result.taxon_photos || [] ).map((p) => {
-                return Object.assign( p.photo, {
-                    commonsCompatLicense:
-                        ['cc-by-sa', 'cc0', 'cc-by'].indexOf(p.photo.license_code) > -1
-                } );
-            } ).sort((p) => p.commonsCompatLicense ? -1 : 1);
-            const name = result.name;
-            return {
-                url: result.wikipedia_url || `https://en.wikipedia.org/wiki/Special:Search?search=${name}`,
-                id,
-                summary: result.wikipedia_summary,
-                name,
-                photos
-            };
+            return flatten( results.map((r) => {
+                return r.photos.map((photo) => {
+                    return Object.assign( photo, {
+                        commonsCompatLicense:
+                            ['cc-by-sa', 'cc0', 'cc-by'].indexOf(photo.license_code) > -1
+                    } );
+                });
+            } ) ).sort((p) => p.commonsCompatLicense ? -1 : 1);
+        } );
+    },
+    fetchTaxa: function ( id ) {
+        return this.fetchPhotos( id ).then( ( photos ) => {
+            return fetch( `https://api.inaturalist.org/v1/taxa/${id}` )
+            .then( (r) => r.json() )
+            .then((j) => {
+                const result = j.results[0] || {};
+                const name = result.name;
+                return {
+                    url: result.wikipedia_url || `https://en.wikipedia.org/wiki/Special:Search?search=${name}`,
+                    id,
+                    summary: result.wikipedia_summary,
+                    name,
+                    photos
+                };
+            } );
         });
     }
 }
