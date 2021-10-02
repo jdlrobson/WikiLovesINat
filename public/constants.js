96db9ee978137ce619ff92774d6a6ec5c6ea5e7f
jdlrobson <jdlrobson@gmail.com>
2021-10-02 09:13:45 -0700

    Add constants for static host

---
+++ public/constants.js
@@ -0,0 +1,6 @@
+const INAT_STATIC_HOST = 'https://inaturalist-open-data.s3.amazonaws.com/';
+const INAT_STATIC_HOST_BACKUP = 'https://static.inaturalist.org/';
+export {
+    INAT_STATIC_HOST,
+    INAT_STATIC_HOST_BACKUP
+};
